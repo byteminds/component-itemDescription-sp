846fd6920075f3a376247ec1b7349777b264f4e1
sparksparker <sehyunspark@gmail.com>
2019-06-21 00:22:05 -0700

    Add favicon icon to page

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+app.use(favicon(path.join(__dirname, '../client/src', 'favicon.ico')));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
