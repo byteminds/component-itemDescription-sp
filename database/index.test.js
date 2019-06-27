69d7f18cc1f27c3d4794ea5881b8851edcfaaec8
sparksparker <sehyunspark@gmail.com>
2019-06-27 14:14:05 -0700

    Test for existing db

--- database/index.test.js
+++ database/index.test.js
@@ -25,4 +25,36 @@ describe('insert', () => {
     const insertedUser = await users.findOne({_id: 'some-user-id'});
     expect(insertedUser).toEqual(mockUser);
   });
+
 });
+
+describe('validate', () => {
+  let db;
+
+  beforeAll(async () => {
+    connection = await mongoose.connect('mongodb://localhost/itemDescription', {
+      useNewUrlParser: true,
+    });
+    db = await mongoose.connection;
+  });
+
+  afterAll(async done => {
+    await db.close();
+    done();
+  });
+
+  it('should validate 100 documents', async () => {
+    let data;
+
+    const queryDocs = db.collection('descriptions')
+                        .count({})
+
+    const numOfDocs = queryDocs
+                        .then( num => data = num)
+                        .catch( () => console.log('promise error') );
+
+    await numOfDocs;
+
+    expect(data).toBe(100);
+  });
+})
