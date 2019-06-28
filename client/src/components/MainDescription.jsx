c11d0c5e97c1d93a18852535be337e66c96a647e
sparksparker <sehyunspark@gmail.com>
2019-06-27 17:57:54 -0700

    Render data

--- client/src/components/MainDescription.jsx
+++ client/src/components/MainDescription.jsx
@@ -2,8 +2,69 @@ import React from 'react';
 
 const MainDescription = ({ data }) => {
   return (
-    <div>
+    <div className='description-container'>
       {console.log('MainD', data)}
+
+      <div className='description'>
+        {data.productName}
+      </div>
+
+      <div className='seller'>
+        by
+        {' '}
+        <span className='blue-text'>
+          {data.seller}
+        </span>
+      </div>
+
+      <img className='stars' src={data.images.stars} />
+
+      <div className='reviews-and-questions'>
+        {data.numOfReviews} customer reviews |
+        {' '}
+        {data.numOfQAndA} answered questions
+      </div>
+
+      <div className='price'>
+        Price:
+        {' '}
+        {data.price}
+        {' '}
+        {data.isPrime[0] === '&' ?
+        <span>
+          {data.isPrime}
+        </span> :
+        <img className='is-prime' src={data.isPrime}/>
+        }
+      </div>
+
+      <div>
+        <strong>Note:</strong>
+        {' '}
+        Available at a lower price from
+        {' '}
+        <span className='blue-text'>
+          other sellers
+        </span>
+        , potentially without free Prime shipping.
+      </div>
+
+      <div className='details'>
+        {data.description}
+      </div>
+
+      <span className='blue-text'>
+        Compare with similar items
+      </span>
+
+      <div className='other-prices'>
+        {data.otherCondition} {data.otherSellers}
+      </div>
+
+      <span className='blue-text'>
+        Report incorrect product information
+      </span>
+
     </div>
   );
 };
