d3cd6430177037881d94c0a53384b51e99c50730
sparksparker <sehyunspark@gmail.com>
2019-06-30 15:43:55 -0700

    Implement init image-magnify

--- client/src/components/MainPicture.jsx
+++ client/src/components/MainPicture.jsx
@@ -1,55 +1,32 @@
 import React, { Component } from 'react';
 import Gallery from 'react-amazon-gallery';
-import Slider from 'react-slick';
+import ReactImageMagnify from 'react-image-magnify';
 import * as Styled from './styled.jsx';
 
 const MainPicture = ({ data }) => {
   let mainPic = data.images.mainImages;
 
-  // render() {
-    return (
-      <div>
-        <Gallery images={mainPic} />
-      </div>
-    )
-  // }
-}
-
-// class MainPicture extends Component {
-
-//   render() {
-//     let data = this.props.data;
-//     let mainPic = data.images.mainImages
-
-//     const settings = {
-//       customPaging: i => {
-//         return (
-//           <a>
-//             <Styled.ImageDot src={mainPic[0 + i]} />
-//           </a>
-//         )
-//       },
-//       dots: true,
-//       dotsClass: 'slick-dots slick-thumb',
-//       infinite: true,
-//       speed: 500,
-//       slidesToShow: 1,
-//       slidesToScroll: 1,
-//       arrows: false,
-//       vertical: true,
-//     };
-
-//     return (
-
-//         <Slider {...settings}>
-//           {mainPic.map(image => (
-//             <div key={mainPic.indexOf(image)} >
-//               <Styled.Image src={image} />
-//             </div>
-//           ))}
-//         </Slider>
-//     );
-//   }
-// };
+  return (
+    <div>
+      <Gallery images={mainPic} />
+        {mainPic.map(image => (
+          <div>
+            <ReactImageMagnify {...{
+              smallImage: {
+                alt: 'Testing for first Magnify',
+                isFluidWidth: true,
+                src: image,
+              },
+              largeImage: {
+                src: image,
+                width: 1200,
+                height: 1800,
+              },
+            }} />
+          </div>
+        ))}
+    </div>
+  )
+};
 
 export default MainPicture;
